abad7db115d9500c8614259f86678b2848e85b36
gman <218506606+gman-comicdb@users.noreply.github.com>
2025-09-25 01:17:53 +0100

    Added `ButtonBase` control loop in `OnLoad`. This fixes an issue where a Form's direct children were not themed (OK, Cancel etc).
    Not sure if there are other controls that can be handled at this level. Likely yes - it would be good to identify. Thie would allow theming to be applied in one place.

--- cYo.Common.Windows/Forms/FormEx.cs
+++ cYo.Common.Windows/Forms/FormEx.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using cYo.Common.Win32;
 using cYo.Common.Windows.Forms.ColorScheme;
+using System.Linq;
 
 namespace cYo.Common.Windows.Forms
 {
@@ -13,7 +14,17 @@ namespace cYo.Common.Windows.Forms
             base.OnHandleCreated(e);
             ColorSchemeExtensions.SetColorScheme(this);
             UXTheme.ApplyDarkThemeToWindow(this, ColorSchemeExtensions.IsDarkModeEnabled, recurse: true);
+        }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            //ColorSchemeExtensions.SetColorScheme(this);
+            //Form buttons were not being themed correctly, presumably because there was no handle to them
+            foreach (ButtonBase button in this.Controls.OfType<ButtonBase>())
+            {
+                UXTheme.ApplyDarkThemeToControl(button, ColorSchemeExtensions.IsDarkModeEnabled);
+            }
         }
     }
 }
