eb318717d2e9b4e198eb8990e9da063b7d1f2030
gman <218506606+gman-comicdb@users.noreply.github.com>
2025-10-29 18:12:09 +0000

    Moved RendererImage and RendererGdiImage from `cYo.Common.Presentation` to `cYo.Common.Drawing`.
    This allows removing ComicRack.Engine dependency on `cYo.Common.Presentation`.

---
+++ cYo.Common/Drawing/RendererGdiImage.cs
@@ -0,0 +1,19 @@
+﻿using cYo.Common.ComponentModel;
+using System;
+using System.Drawing;
+
+namespace cYo.Common.Drawing;
+
+public class RendererGdiImage : RendererImage
+{
+    private readonly WeakReference<Bitmap> weakReference;
+
+    public override bool IsValid => weakReference.IsAlive();
+
+    public override Bitmap Bitmap => weakReference.GetData();
+
+    public RendererGdiImage(Bitmap bitmap)
+    {
+        weakReference = new WeakReference<Bitmap>(bitmap);
+    }
+}
